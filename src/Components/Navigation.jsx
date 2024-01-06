7c3a0556958e0e0a9ad7b17e9565ec58b88b7024
mohammad shaheer <shaheercse2022@gmail.com>
2024-01-06 23:22:46 +0530

    login-signup routing

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link, Outlet  } from "react-router-dom";
 import { FaApple } from "react-icons/fa";
 import { AiOutlineMenu } from "react-icons/ai";
 import { FaRegUser } from "react-icons/fa6";
@@ -13,32 +14,35 @@ const Navigation = () => {
 
   return (
     <>
-      <div className="">
+      <div>
         <div>
           <nav className="flex justify-between items-center py-2 px-4 bg-gray-100 shadow-md shadow-gray-200 fixed top-0 left-0 right-0 z-10">
             <div className="md:flex md:items-center space-x-10">
               <div className="flex items-center gap-2">
-              <FaApple /> 
+                <FaApple />
                 <h3 className="text-lg font-bold tracking-widest uppercase">
-                    Apple
+                  Apple
                 </h3>
               </div>
               <div className="hidden md:block md:space-x-5 md:font-medium md:text-sm">
-                <a className="hover:text-gray-500 px-3" href="">
+                <Link className="hover:text-gray-500 px-3" to="/">
                   Home
-                </a>
-                <a className="hover:text-gray-500 px-3" href="">
+                </Link>
+                <Link className="hover:text-gray-500 px-3" to="/shop">
                   Shop
-                </a>
-                <a className="hover:text-gray-500 px-3" href="">
+                </Link>
+                <Link className="hover:text-gray-500 px-3" to="/cart">
                   Cart
-                </a>
+                </Link>
               </div>
             </div>
             <div className="flex space-x-5">
               <FiShoppingCart className="cursor-pointer md:text-xl" />
-              <FaRegUser className="cursor-pointer md:text-xl" />
-              <button onClick={openMenu} className="block focus:outline-none md:hidden">
+              <Link to="/login"><FaRegUser className="cursor-pointer md:text-xl" /></Link>
+              <button
+                onClick={openMenu}
+                className="block focus:outline-none md:hidden"
+              >
                 {isOpen ? <IoClose /> : <AiOutlineMenu />}
               </button>
             </div>
@@ -46,16 +50,16 @@ const Navigation = () => {
           {isOpen ? (
             <div>
               <div className="h-screen text-lg font-medium flex flex-col gap-20 justify-center items-center bg-white fixed top-0 left-0 right-0 md:hidden">
-              {/* <div className={`${isOpen ? 'h-screen' : 'h-0 overflow-hidden'} transition-all duration-500 ease-out`}> */}
-                <a className="hover:text-gray-500" href="">
+                {/* <div className={`${isOpen ? 'h-screen' : 'h-0 overflow-hidden'} transition-all duration-500 ease-out`}> */}
+                <Link className="hover:text-gray-500" to="/">
                   Home
-                </a>
-                <a className="hover:text-gray-500" href="">
+                </Link>
+                <Link className="hover:text-gray-500" to="/shop">
                   Shop
-                </a>
-                <a className="hover:text-gray-500" href="">
+                </Link>
+                <Link className="hover:text-gray-500" to="/cart">
                   Cart
-                </a>
+                </Link>
               </div>
             </div>
           ) : (
@@ -63,6 +67,7 @@ const Navigation = () => {
           )}
         </div>
       </div>
+      <Outlet />
     </>
   );
 };
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,3 +1,4 @@
+import { Link} from "react-router-dom";
 import { FaGoogle, FaApple, FaFacebookF } from "react-icons/fa";
 const SignUp = () => {
     return(
@@ -5,16 +6,17 @@ const SignUp = () => {
             <div>
                 <div className="h-screen w-screen flex flex-col justify-center items-center space-y-10">
                     <div>
-                        <h1 className="text-5xl font-semibold text-gray-600 md:text-6xl md:mb-10">Sign Up</h1>
+                        <h1 className="text-5xl font-semibold text-gray-600 md:mt-10">Sign Up</h1>
                     </div>
                     <form className="flex flex-col space-y-5 overflow-hidden">
                         <input className="w-64 bg-gray-100 font-medium placeholder:text-sm  focus:outline-none p-2 md:w-80" type="text" placeholder="Name" />
                         <input className="w-64 bg-gray-100 font-medium placeholder:text-sm  focus:outline-none p-2 md:w-80 " type="email" placeholder="Email" />
                         <input className="w-64 bg-gray-100 font-medium placeholder:text-sm  focus:outline-none p-2 md:w-80" type="password" placeholder="Password" />
                         <input className="text-white rounded-sm font-semibold cursor-pointer p-2 bg-blue-500 active:bg-blue-600 hover:bg-blue-400" type="submit" value={"Sign Up"} />
-                        <p className="text-sm font-medium text-center">Already have an account? <a className="text-blue-500" href="">Sign In</a></p>
+                        <p className="text-sm font-medium text-center text-gray-500">Already have an account? <Link className="text-blue-500" to="/login">Login</Link></p>
                     </form>
-                    <div className="text-xs text-gray-500">
+                    
+                    <div className="text-xs text-gray-500 md:text-sm">
                         <p className="grid grid-cols-3 items-center gap-2 before:h-[2px] before:bg-gray-200 before:block after:h-[2px] after:bg-gray-200 after:block ">or SignUp with</p>
                     </div>
                     <div className="max-w-md grid grid-cols-3 items-center gap-1 justify-between">
